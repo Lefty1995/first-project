d7fd5d6b577dd1e13b838d00b49825991a2e2cd0
Enrico <enrico.digitalmarketing@gmail.com>
2025-11-18 12:41:52 +0100

    esercitazione_1

---
+++ esercitazione_1.py
@@ -0,0 +1,66 @@
+#Esercizio 1
+nome = 'enrico'
+eta = 30
+citta = 'milano'
+print(nome,eta,citta)
+
+#Esercizio 2
+x = 10
+print(x)    
+x = 30
+print(x)
+
+#Esercizio 3
+x = 50
+y = 50
+somma = x + y
+print(somma)
+
+#Esercizio 4
+x = 20
+y = 15
+x,y = y,x
+print(x)
+print(y)
+
+#Esercizio 5
+base = 5
+altezza = 10
+area = (base * altezza) / 2
+print('area rettangolo:', area)
+
+#Esercizio 6
+a = 10
+b = 3.5
+print(a+b)
+
+#Esercizio 7
+x , y , z = 10 , 60 , 90 
+media = (x+y+z) / 3 
+print('Media' , media)
+
+#Esercizio 8
+s1 = 'mi piace'
+s2 = ' giocare alla playstation'
+print(s1 + s2) 
+
+#Esercizio 9
+print ( "ciao " * 3)
+
+#Esercizio 10
+a = 15 
+b = 30 
+c = 45
+print ( a < b )
+print ( b > c )
+print ( a < c )
+
+#Esercizio 11
+x = 10 
+y = float(x)
+print(y)
+
+#Esercizio 12
+n = 4550
+s = str(n)
+print('il numero è' , s)  
