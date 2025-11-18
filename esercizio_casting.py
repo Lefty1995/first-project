a7bd6a031a08d47f97f21f3fdbecd9aa91ccfa75
Enrico <enrico.digitalmarketing@gmail.com>
2025-11-18 12:05:48 +0100

    esercizio_casting.py

---
+++ esercizio_casting.py
@@ -0,0 +1,18 @@
+#Esercizio 1 
+
+str = 'Inserisci un numero intero: 30'
+print(str)
+
+#Esercizio 2
+
+str = '30' 
+n_int = int(str)
+print(n_int)
+
+#Esercizio 3
+
+n_int = 30
+str = str(n_int)
+str_2 = 'Il numero intero è:'
+
+print(str_2 , str)
