6d7949f3ea782eeafa78744b461dac88984ee800
Enrico <enrico.digitalmarketing@gmail.com>
2025-11-27 16:48:38 +0100

    Update esercitazione_ciclo_for.py

--- esercitazione_ciclo_for.py
+++ esercitazione_ciclo_for.py
@@ -23,3 +23,38 @@ for i in range (1, 11):
    print(f" 2 x {i} = {2*i}")
    
 #Esercizio 6
+n = 5
+fattoriale = 1
+for i in range (1, n+1):
+   fattoriale *=i
+   print("Fattoriale" , fattoriale)
+
+#Esercizio 7
+parola = "programmazione"
+vocali = "aeiou"
+conta = 0 
+for c in parola:
+   if c in vocali:
+      conta +=1
+print("Numero di vocali in", parola, "è:", conta)
+
+#Esercizio 8
+for numero in range (1, 4):
+   for j in range (1, 4):
+      print(f"{numero} x {j} = {numero*j}")
+print()
+
+#Esercizio 9
+for i in range (1, 11): 
+   if i == 5: 
+        continue
+print(i)
+
+#Esercizio 10
+for i in range (1, 11):
+   if i ==7:
+        break
+print(i)
+
+
+
