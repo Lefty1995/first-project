aad3e443f757b2b8a774e15c0d460119dbb96c63
Enrico <enrico.digitalmarketing@gmail.com>
2025-11-27 16:47:58 +0100

    esercitazioni_modulo_ciclo_for.py

---
+++ esercitazione_ciclo_for.py
@@ -0,0 +1,25 @@
+#Esercizio 1
+for i in range (1,11):
+ print(i)
+
+#Esercizio 2
+for i in range ( 2, 21, 2):
+    print(i)
+
+#Esercizio 3
+parola = "python"
+for lettera in parola:
+    print(lettera)
+
+#Esercizio 4
+somma = 0
+for i in range (1, 101):
+    somma += i
+print("La somma dei numeri da 1 a 100 è:", somma)
+
+#Esercizio 5
+for i in range (1, 11):
+   print(f" 5x {i} = {5*i}")
+   print(f" 2 x {i} = {2*i}")
+   
+#Esercizio 6
---
+++ esercizi_ciclo_for.py
@@ -0,0 +1,3 @@
+names = ["Enrico", "Alessia", "Antonio", "Beppe"]
+for index, name in enumerate(names, start=1):
+    print(f"{index}. {name}")
